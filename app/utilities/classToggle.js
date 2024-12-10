1a169b544617b8266245afe817f82509fe216722
Carlos Guanipa <carlosguanipa23@gmail.com>
2024-12-10 16:54:46 -0400

    Correcciones de vistas

--- app/utilities/classToggle.js
+++ app/utilities/classToggle.js
@@ -1,8 +1,10 @@
-//Funcion de alternacion de clases
+//Funcion de alternacion de clases de acordeon
 
 export function classToggle(elements, classes) {
   const active = (e) => {
-    e.currentTarget.classList.toggle(classes);
+    e.target.matches("p") ||
+      e.target.matches("span") ||
+      e.currentTarget.classList.toggle(classes);
   };
   elements.forEach((el) => el.addEventListener("click", active));
 }
